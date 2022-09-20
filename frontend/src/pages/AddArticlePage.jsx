c8284c3aa69e92334f9662bbe1fb38ce9f365a5e
nukmansalim <nukmansaleem@gmail.com>
2022-09-21 01:43:48 +0700

    Making Add Article Page & Tag List

--- frontend/src/pages/AddArticlePage.jsx
+++ frontend/src/pages/AddArticlePage.jsx
@@ -1,8 +1,88 @@
 import React from 'react'
+import { useState } from 'react'
 
 function AddArticle() {
+    const [Tags, setTags] = useState([])
+    const [createTags, setCreateTags] = useState("")
+
+    // const handleTags = (e) => {
+    //     e.preventDefault()
+    //     setTags(createTags)
+    // }
+
+    const addTags = () => {
+        Tags.push(createTags)
+        setCreateTags('')
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
     return (
-        <div>AddArticle</div>
+        <div className="container box p-6
+        has-background-light">
+            <h1 className="title has-text-centered
+         has-text-info">
+                Publish your articles
+            </h1>
+            <form onSubmit={handleSubmit} action="">
+                <div className="field">
+                    <label className="label">Title</label>
+                    <div className="control">
+                        <input className="input" type="text" />
+                    </div>
+                </div>
+
+
+                <div className="field">
+                    <label className="label">
+                        Something Else
+                    </label>
+                    <div className="control">
+                        <textarea
+                            className="textarea"
+                        ></textarea>
+                    </div>
+                </div>
+
+                <div className="field">
+                    <label className="label">Article Tags</label>
+                    <div className="control">
+                        <input
+                            onChange={(e) => setCreateTags(e.target.value)}
+                            className="input"
+                            type="text" />
+                        <div className=''>
+                            {Tags.map((Tag, i) => {
+                                return (
+                                    <button className='button is-info m-2' key={i}>
+                                        {Tag}
+
+                                    </button>
+                                )
+                            })}
+                        </div>
+                        <button
+                            onClick={addTags}
+                            type='submit'
+                            className='mt-3 button is-info'>Add Tags</button>
+
+                    </div>
+                </div>
+
+
+
+
+
+                <div className="field is-grouped">
+                    <div className="control">
+                        <button className="button is-success">
+                            Add Article
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
     )
 }
 
