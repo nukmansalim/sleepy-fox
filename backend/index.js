de94f0d471e21826c2e040da77feb9a441bd0f50
nukmansalim <nukmansaleem@gmail.com>
2022-09-22 14:37:48 +0700

    implementing Google Oauth 2.0 using passport-google-oauth20

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,16 @@ const mongoose = require("mongoose")
 const app = express()
 const PostRoutes = require('./routes/PostRoutes')
 const UserRoutes = require('./routes/UserRoutes')
-const PORT = 8080 || process.env.PORT
-
-
+const passport = require("passport")
+require('./strategies/google.js')
+require('dotenv').config()
 mongoose.connect("mongodb://localhost:27017/MERN-Web", () => console.log("database is connected"))
 
 app.use(express.json())
 app.use(cors())
-
+app.use(passport.initialize())
 app.use("/posts", PostRoutes)
 app.use("/auth", UserRoutes)
 
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+app.listen(process.env.PORT, () => console.log(`server is running on port`))
 
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -5,8 +5,11 @@ const passport = require('passport')
 const router = express.Router()
 
 
-
 router.get('/google', passport.authenticate('google'), (req, res) => {
+    res.send(200)
+}
+)
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
     res.send(200)
 }
 )
