119431bc58ba1623d14c12b0757e6c0de0c8f02a
nukmansalim <nukmansaleem@gmail.com>
2022-09-22 16:26:11 +0700

    removing oauth login features

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,31 @@ const mongoose = require("mongoose")
 const app = express()
 const PostRoutes = require('./routes/PostRoutes')
 const UserRoutes = require('./routes/UserRoutes')
+const session = require('express-session')
 const passport = require("passport")
-require('./strategies/google.js')
 require('dotenv').config()
+
+//connect mongoose
 mongoose.connect("mongodb://localhost:27017/MERN-Web", () => console.log("database is connected"))
 
+
+//passport config
+require('./strategies/google.js')(passport)
+
+//express basic middleware
 app.use(express.json())
 app.use(cors())
+app.use(session({
+    secret: 'blogweb',
+    resave: 'false',
+    saveUninitialized: true
+}))
+
+//passport middleware
 app.use(passport.initialize())
+app.use(passport.session())
+
+//custom middleware
 app.use("/posts", PostRoutes)
 app.use("/auth", UserRoutes)
 
